058c8c74bd40a41bfbfa6f6dbbc9bb7f8c725720
Skowoo <skowoo@tlen.pl>
2024-05-31 14:06:13 +0200

    AdminPanel layout prototype implemented

---
+++ ClientApplication/Pages/AdminPanel/DeleteRole.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ClientApplication.Pages.AdminPanel
+{
+    public class DeleteRoleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
