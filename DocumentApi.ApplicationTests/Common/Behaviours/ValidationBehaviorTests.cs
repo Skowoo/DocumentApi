59aae9a109b47830de9b82dfbba4598ee722af0f
Skowoo <skowoo@tlen.pl>
2024-06-07 14:17:12 +0200

    Tests naming convention updated and unit testing for validation behaviour added

---
+++ DocumentApi.ApplicationTests/Common/Behaviours/ValidationBehaviorTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using Moq;
+using FluentValidation;
+using DocumentApi.Application.Translators.Commands.CreateTranslator;
+
+namespace DocumentApi.Application.Common.Behaviours.Tests
+{
+    public class ValidationBehaviorTests
+    {
+        readonly CreateTranslatorCommandValidator CommandValidator = new();
+        readonly Mock<MediatR.RequestHandlerDelegate<CreateTranslatorCommand>> nextDelegateMock = new();
+
+        [Fact]
+        public async Task ShouldThrowValidationError()
+        {
+            var testedBehaviour = new ValidationBehavior<CreateTranslatorCommand, CreateTranslatorCommand>([CommandValidator]);
+            var command = new CreateTranslatorCommand() { Name = "" };
+            await Xunit.Assert.ThrowsAsync<ValidationException>(async () => await testedBehaviour.Handle(command, nextDelegateMock.Object, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task ShouldNotThrowValidationError()
+        {
+            var testedBehaviour = new ValidationBehavior<CreateTranslatorCommand, CreateTranslatorCommand>([CommandValidator]);
+            var command = new CreateTranslatorCommand() { Name = "ValidName" };
+            await testedBehaviour.Handle(command, nextDelegateMock.Object, CancellationToken.None);
+        }
+    }
+}
